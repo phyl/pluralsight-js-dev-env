b288b0f4cc7edee01ccbb9a09cf04eb115cb0682
Phyllis <pmhindurwa@gmail.com>
2019-07-02 05:28:27 +0200

    020702

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -19,6 +19,16 @@ app.get('/', function(req, res){
   res.sendFile(path.join(__dirname, '../src/index.html'))
 });
 
+
+app.get('/users', function(req, res){
+  //Hard coding for simplicity. Pretends this hits the real database
+  res.json([
+    { "id": 1,"firstName":"Bob","lastName":"Smith","email":"bob@gmail.com"},
+    { "id": 2,"firstName":"Tamay","lastName":"Norton","email":"tnorton@gmail.com"},
+    { "id": 3,"firstName":"Tina","lastName":"Lee","email":"lee.tina@gmail.com"}
+  ]);
+});
+
 app.listen(port, function(err){
   if (err) {
     console.log(err);
