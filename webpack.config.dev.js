666ca37ba331a156e3a359b29c26dc01a6d67a47
P MHINDURWA <47423749@mylife.unisa.ac.za>
2019-07-03 16:33:58 +0200

    030703

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,4 +1,7 @@
 import path from 'path';
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import webpack from 'webpack';
+
 
 export default {
   debug: true,
@@ -11,9 +14,24 @@ export default {
   output: {
     path: path.resolve(__dirname, 'src'),
     publicPath: '/',
-    filename: 'bundle.js'
+    filename: '[name].js'
   },
-  plugins: [],
+  plugins: [
+
+    // Use CommonsChunkPlugin to create a separate bundle
+    // of vendor libraries so that they're cached separately.
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor'
+    }),
+
+
+    // crear HTML file that includes reference to bundled JS
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+      inject: true
+    })
+
+  ],
   module: {
     loaders: [
       {test: /\.js$/, exclude: /node_modules/, loaders: ['babel']},
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,13 +1,15 @@
 import path from 'path';
 import webpack from 'webpack';
+import HtmlWebpackPlugin from "html-webpack-plugin";
 
 export default {
   debug: true,
   devtool: 'source-map',
   noInfo: false,
-  entry: [
-    path.resolve(__dirname, 'src/index')
-  ],
+  entry: {
+    vendor: path.resolve(__dirname, 'src/vendor'),
+    main: path.resolve(__dirname, 'src/index')
+  },
   target: 'web',
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -15,6 +17,25 @@ export default {
     filename: 'bundle.js'
   },
   plugins: [
+
+    // crear HTML file that includes reference to bundled JS
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeRedundantAttributes: true,
+        useShortDoctype: true,
+        removeEmptyAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        keepClosingSlash: true,
+        minifyJS: true,
+        minifyCSS: true,
+        minifyURLs: true
+      },
+      inject: true
+    }),
+
     // Eliminate duplicate packages when generating bundles
     new webpack.optimize.DedupePlugin(),
 
